e2443c8a14e69b9e464193fcfe6656052bd59a3c
Sergio Fernández <sergiofh.2002@gmail.com>
2021-09-21 10:49:00 +0200

    Ejercicio 2 A

---
+++ PR01/Assets/Scripts/Ejercicio2a.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2a : MonoBehaviour
+{
+    int n = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        while (n <= 10)
+        {
+            
+            Debug.Log("n vale " + n);
+            n++;
+
+
+        }
+
+
+    }
+}

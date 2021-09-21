17c21f3c0356031e772a860d4a866d8cf77ac843
Sergio Fernández <sergiofh.2002@gmail.com>
2021-09-21 10:47:18 +0200

    Ejercicio 1 A

---
+++ PR01/Assets/Scripts/Ejercicio1a.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1a : MonoBehaviour
+{
+    string nombre = "Sergio";
+    int numero1 = 5;
+    float numero2 = 40.50f;
+    bool alive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log(nombre);
+        Debug.Log(numero1);
+        Debug.Log(numero2);
+        Debug.Log(alive);
+
+
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
